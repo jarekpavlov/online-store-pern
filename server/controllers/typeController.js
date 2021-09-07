40196eafd42fe8b0af8161b4bbc742010e870484
jarekpavlov <jarekpavlov@gmail.com>
2021-09-07 10:01:18 +0300

    Creating role actions(it works for types in this commit) and other CRUD functionality

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -5,6 +5,7 @@ class TypeController{
         const {name} = req.body
         const type = await Type.create({name})
         return res.json(type)
+
     }
     async getAll(req,res){
         const types = await Type.findAll()
